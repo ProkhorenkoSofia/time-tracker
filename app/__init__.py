05912861783c6f1d506ab6d54121a432308c69e1
Прохоренко Софья <sofia.prokhorenko_1@mail.ru>
2025-12-08 02:46:02 +0300

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,15 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from config import Config
+from app.models import db
 
 db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object('config.Config')
-    
     db.init_app(app)
     
-    # Правильный импорт
     from .routes.main_routes import main_routes
     app.register_blueprint(main_routes)
     
