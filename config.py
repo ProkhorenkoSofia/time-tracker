6a0ddc8fba277883e0a4dc2407928f675f5509fe
Прохоренко Софья <sofia.prokhorenko_1@mail.ru>
2025-12-17 17:08:00 +0300

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 from datetime import timedelta
 
 class Config:
-    SECRET_KEY = os.environ.get('BD_Kursach') or 'BD_Kursach'
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'BD_Kursach'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///time_tracker.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_ECHO = False
